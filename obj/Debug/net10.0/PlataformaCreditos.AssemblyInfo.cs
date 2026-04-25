78f723cd8001665b64047a00676a3ba70102c6d8
LucianaME14 <lucianamorenoesp14@gmail.com>
2026-04-25 13:22:03 -0500

    Modelos Cliente y SolicitudCredito con restricciones

--- obj/Debug/net10.0/PlataformaCreditos.AssemblyInfo.cs
+++ obj/Debug/net10.0/PlataformaCreditos.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlataformaCreditos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ffe09ca3335d7216f201045667e042354c806c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlataformaCreditos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlataformaCreditos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
